5365a144f884a27cef6c3d5342d923a18ea60ae8
trungld3 <k21tpm4@gmail.com>
2020-05-22 15:15:54 +0700

    Initial Commit

---
+++ Location Aware/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Location Aware
+//
+//  Created by TrungLD on 5/22/20.
+//  Copyright © 2020 TrungLD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
